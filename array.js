8be16d2ebd2605e44dfb37a0f665060cecd43326
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-07-30 13:52:13 -0400

    Update Array method

--- array.js
+++ array.js
@@ -12,7 +12,6 @@ let cont=0
 let qt=students.length
 
 while (cont<qt){
-    
         console.log(students[cont].age)
         console.log("========")
     cont ++ 
@@ -20,6 +19,7 @@ while (cont<qt){
 
 // Function return Array
 //let sqrtage=Math.sqrt(students[cont].age)
-let ages=students.map(student => Math.sqrt(student.age))
+        console.log("The Square of Array is: ")
+let ages=students.map(student => Math.sqrt(student.age).toFixed(2))
     console.log(ages)
 
