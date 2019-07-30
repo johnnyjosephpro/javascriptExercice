ae1d274cba4ab815224de24c13798763bd34c3d3
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-07-30 14:38:26 -0400

    Mofiy functin in JJ file

--- jj.js
+++ jj.js
@@ -1,7 +1,14 @@
-let firstName = "jackson"
-let lastName = "jules"
-let fullName = firstName + lastName
-console.log (fullName)
+//Voici comment on dit Je t'aime en programmation :)
+
+let per=["Jacquelines","Eveline","Martine","Nerette"]
+
+    console.log(per[0].charAt(0)+per[1].charAt(2)+" "+per[2].charAt(3)+"'"+per[2].charAt(1)
+                +per[0].charAt(7)+per[2].charAt(0).toLowerCase()+per[3].charAt(6))
+
+
+
+
+
 
 
 
