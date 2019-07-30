c2e1d33b42f98112b3804840cad2deb2a228446b
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-07-30 11:38:44 -0400

    Add file Array

--- array.js
+++ array.js
@@ -1,4 +1,27 @@
-let arr=[1,2,3,4,5]
+//let arr=[1,2,3,4,5]
+//console.log(arr[1])
 
-console.log(arr[1])
 
+let students = [
+    {name: "Jephte", age: 27},
+    {name: "Jean", age: 18},
+    {name: "Max", age: 19},
+    {name: "Pierre", age: 14},
+    {name: "Joseph", age: 16},
+]
+
+let cont=0
+let qt=students.length
+
+
+while (cont<qt){
+    console.log(students[cont].age)
+        console.log("========")
+    cont ++ 
+}
+
+
+
+let ages=students.map(student => student.age)
+
+console.log(ages)
