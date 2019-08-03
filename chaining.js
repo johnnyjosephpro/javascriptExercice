9a0e4057c4d53294d05e645839040044d78f5bbd
Johnny Joseph <johnnyjosephpro@gmail.com>
2019-08-03 16:07:12 -0400

    Add method filter

--- chaining.js
+++ chaining.js
@@ -3,8 +3,22 @@ let name= "javascript"
 let arr = name.split("")
 let rev= arr.reverse()
 let join= rev.join("")
-
+console.log("===============================================")
 //Print result
 console.log(rev)
-console.log(join)
+    console.log(join)
+
+// Using an other method
+                   console.log(name.split("")
+                   .reverse()
+                   .join(""))
+                   
+
+
+
+
+
+
+
+
 
--- reduce.js
+++ reduce.js
@@ -14,10 +14,35 @@ let par= students.length
 let totalAge=0
 
 for(i=0; i<par ; i++){
-    totalAge = totalAge + students[i].age
-} 
+                      totalAge = totalAge + students[i].age
+             } 
 console.log(totalAge)
+
+//ES6 MAP
+let ageOf = students.map(student => student.age)
+console.log(ageOf)
+
+// ES5 MAP
+let ageOfStud = students.map(function(e){
+    return e.age;
+})
+
+console.log(ageOfStud)
 
 //How to calculate the average of students ages
 //Reduce ((acc,val,index,arr),initValue)
+
+console.log("====================== Chaining method ==========================")
+
+let totalAgeRev = students.map(student => student.age).filter(age => age ==20).reduce((acc,val)=>{
+    return acc + val
+})
+
+console.log(totalAgeRev)
+
+console.log("====================== Add 2 for all age students ==========================")
+
+let Addage = students.map(student => student.age+2)
+
+console.log(Addage)
 
